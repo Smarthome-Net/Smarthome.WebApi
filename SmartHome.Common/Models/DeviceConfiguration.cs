e934b852ebbdeca428c9a42b8351352ade3e31fc
Stephan F. <Stephan.feige@yahoo.com>
2024-03-10 22:58:26 +0100

    Update Model

--- SmartHome.Common/Models/DeviceConfiguration.cs
+++ SmartHome.Common/Models/DeviceConfiguration.cs
@@ -2,18 +2,11 @@
 
 public class DeviceConfiguration
 {
-    public string IP { get; set; }
-    public string DNS { get; set; }
-    public string Subnet { get; set; }
-    public bool Dhcp { get; set; }
-    public string MqttTopic { get; set; }
     public string Name { get; set; }
-
-    public string MqttServer { get; set; }
-    public string MqttServerPassword {get; set;}
-    public string MqttServerCertificateKey { get; set; }
-
-    public string WlanSsid { get; set; }
-    public string WlanPassword { get; set; }
-    public string WlanCertificateKey { get; set; }
+    public string Room { get; set; }
+    public int Interval { get; set; }
+    public string MqttHost { get; set; }
+    public int MqttPort { get; set;}
+    public string Ssid { get; set; }
+    public string SsidPassword { get; set; }
 }
