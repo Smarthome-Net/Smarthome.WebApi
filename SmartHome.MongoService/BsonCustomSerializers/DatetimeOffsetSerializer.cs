619304b58558fef926eb39239e29db4dcc894ec4
Stephan F <Stephan.feige@yahoo.com>
2025-02-12 13:09:07 +0100

    Added some comments

--- SmartHome.MongoService/BsonCustomSerializers/DatetimeOffsetSerializer.cs
+++ SmartHome.MongoService/BsonCustomSerializers/DatetimeOffsetSerializer.cs
@@ -1,19 +1,33 @@
 using System;
-using System.Globalization;
 using MongoDB.Bson.Serialization;
 
 namespace SmartHome.MongoService.BsonCustomSerializers;
 
+/// <summary>
+/// Custom Date time offset serializer for time series
+/// </summary>
 public class DatetimeOffsetSerializer : IBsonSerializer<DateTimeOffset>
 {
     public Type ValueType => typeof(DateTimeOffset);
 
+    /// <summary>
+    /// Deserialization only UTC dates, to prevent any logical error when handling with dates, the Client should format the date
+    /// </summary>
+    /// <param name="context"></param>
+    /// <param name="args"></param>
+    /// <returns></returns>
     public DateTimeOffset Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
     {
         var value = context.Reader.ReadDateTime();
         return DateTimeOffset.FromUnixTimeMilliseconds(value);
     }
 
+    /// <summary>
+    /// Serialization into datetime with unix milliseconds. Note: The date will be transformed from local into UTC date
+    /// </summary>
+    /// <param name="context"></param>
+    /// <param name="args"></param>
+    /// <param name="value"></param>
     public void Serialize(BsonSerializationContext context, BsonSerializationArgs args, DateTimeOffset value)
     {
         var rawValue = value.ToUnixTimeMilliseconds();
